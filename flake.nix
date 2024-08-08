a10c8234152e55c5502b2250133fc13b1e0bc52d
Sam Bossley <sam@bossley.xyz>
2024-08-08 07:56:25 -0400

    feat(packages): update dev flake

--- flake.nix
+++ flake.nix
@@ -3,22 +3,24 @@
 
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-
     node_nixpkgs.url = "github:nixos/nixpkgs/a343533bccc62400e8a9560423486a3b6c11a23b"; # v20.11.1
   };
 
   outputs =
-    { self, flake-utils, nixpkgs, node_nixpkgs }: flake-utils.lib.eachDefaultSystem (system:
+    inputs: inputs.flake-utils.lib.eachDefaultSystem (system:
     let
-      pkgs = import nixpkgs { inherit system; };
+      pkgs = import inputs.node_nixpkgs { inherit system; };
     in
     {
       devShells.default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          node_nixpkgs.legacyPackages.${system}.nodejs
-          node_nixpkgs.legacyPackages.${system}.nodePackages.yarn
-          prettierd
+        buildInputs = [
+          pkgs.nodejs
+          pkgs.nodePackages.yarn
+          # LSP
+          pkgs.prettierd
+          pkgs.nodePackages.vscode-langservers-extracted
+          pkgs.nodePackages.typescript-language-server
+          pkgs.efm-langserver
         ];
         shellHook = ''
           set -o vi
