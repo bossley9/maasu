99827093b610facb5744a8484d684f3150bff9e3
Sam Bossley <sam@bossley.xyz>
2024-08-15 20:09:51 -0400

    feat: modify project for sjsx

---
+++ main.ts
@@ -0,0 +1,22 @@
+import fs from "node:fs";
+import path from "node:path";
+
+async function* walk(dir: string): AsyncGenerator<string> {
+  for await (const d of await fs.promises.opendir(dir)) {
+    const entry = path.join(dir, d.name);
+    if (d.isDirectory()) yield* walk(entry);
+    else if (d.isFile() && d.name.endsWith(".tsx")) yield entry;
+  }
+}
+
+async function main() {
+  for await (const page of walk("./src/pages")) {
+    const output = "./dist" + page.substring(9, page.length - 3) + "html";
+    fs.mkdirSync(output.substring(0, output.lastIndexOf("/")), {
+      recursive: true,
+    });
+    fs.writeFileSync(output, (await import("./" + page)).default());
+  }
+}
+
+main();
