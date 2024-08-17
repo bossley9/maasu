5e4c0c84f4e95f89b9096e2cfd758b8d43062f52
Sam Bossley <sam@bossley.xyz>
2024-08-17 14:42:12 -0400

    feat: update contact form

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -3,38 +3,6 @@ import { TextBlock } from "../../components/TextBlock";
 import { BaseLayout } from "../../layouts/BaseLayout";
 
 export default function () {
-  const submissionMsg = "";
-  // const [submissionMsg, setSubmissionMsg] = useState('')
-
-  // const handleFormSubmit = async (e: React.FormEvent) => {
-  //   e.preventDefault()
-  //
-  //   const { name, email, message } = e.target as any
-  //
-  //   const response = {
-  //     'entry.761143299': name.value,
-  //     'entry.390586168': 'board@maasu.org',
-  //     'entry.1997635310': email.value,
-  //     'entry.596068453': message.value,
-  //   }
-  //
-  //   const formData = querystring.stringify(response)
-  //
-  //   const url = `https://docs.google.com/forms/d/e/1FAIpQLSc17EG7ml0EG9Kb6JYED6lIaZhaDKZmK3NxPmOjp1vErzo0Ag/formResponse?${formData}`
-  //
-  //   const options = {
-  //     method: 'POST',
-  //     mode: 'no-cors',
-  //   }
-  //
-  //   try {
-  //     await fetch(url, options as RequestInit)
-  //     setSubmissionMsg('Your message was sent.')
-  //   } catch (e) {
-  //     setSubmissionMsg(`Unable to send message: ${e.message}`)
-  //   }
-  // }
-
   const inputClassNames =
     "clearall w-100 fs0 my3 pa3 ff-hind bg-bg-main border bs-solid bc-text-main";
 
@@ -46,42 +14,74 @@ export default function () {
             Any questions? Send us a message and we'll get back to you as soon
             as possible!
           </p>
-          {/* <form class="mxa w-100" onSubmit={handleFormSubmit}> */}
-          <form class="mxa w-100">
+          <form id="contact" class="mxa w-100" method="post">
+            <input
+              type="text"
+              required
+              readonly
+              name="entry.390586168"
+              value="board@maasu.org"
+              style="display:none!important"
+            />
             <input
               class={inputClassNames}
               type="text"
               required
-              name="name"
+              name="entry.761143299"
               placeholder="First and last name"
             />
             <input
               class={inputClassNames}
               type="email"
               required
-              name="email"
+              name="entry.1997635310"
               placeholder="Email"
             />
             <textarea
               class={inputClassNames}
               required
-              name="message"
+              name="entry.596068453"
               placeholder="Message"
               style="min-height:300px"
             />
-            {submissionMsg ? (
-              <div class={inputClassNames}>{submissionMsg}</div>
-            ) : (
-              <button
-                class={`${inputClassNames} fw700 tr-2 c-bg-main bg-text-main hov-c-text-main hov-bg-bg-main`}
-                type="submit"
-              >
-                Submit
-              </button>
-            )}
+            <div id="submsg" class={inputClassNames} style="display:none"></div>
+            <button
+              id="contact-submit"
+              class={`${inputClassNames} fw700 tr-2 c-bg-main bg-text-main hov-c-text-main hov-bg-bg-main`}
+              type="submit"
+            >
+              Submit
+            </button>
           </form>
         </TextBlock>
       </Container>
+      <script>
+        {`
+        function setMessage(message) {
+          document.getElementById("contact-submit").remove()
+          const msg = document.getElementById("submsg")
+          msg.innerHTML = message
+          msg.style.display = "block"
+        }
+
+        document.getElementById("contact").addEventListener("submit", function(e) {
+          e.preventDefault()
+          const params = new URLSearchParams(new FormData(e.target))
+          const url = "https://docs.google.com/forms/d/e/1FAIpQLSc17EG7ml0EG9Kb6JYED6lIaZhaDKZmK3NxPmOjp1vErzo0Ag/formResponse?" + params.toString()
+
+          fetch(url, { method: "POST", mode: "no-cors" })
+            .then(function (res) {
+              if (res.status >= 400) {
+                throw new Error(res.statusText)
+              }
+              setMessage("Your message was sent.")
+            })
+            .catch(function(e) {
+              setMessage("Unable to send message: " + e.message)
+            })
+        })
+        `}
+      </script>
     </BaseLayout>
   );
 }
